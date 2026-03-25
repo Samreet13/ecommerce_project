e0777e5177dd87238a467c7070cc3fd3a20d945b
Samreet13 <skaur228@rrc.ca>
2026-03-25 13:02:50 -0500

    Add model associations, validations, and scopes for all models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,10 @@
 class Customer < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   belongs_to :province, optional: true
   has_many :addresses
   has_many :orders
 
   validates :first_name, :last_name, presence: true
+  validates :phone, format: { with: /\A[\d\s\-\+\(\)]+\z/ }, allow_blank: true
 end
